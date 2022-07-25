47c4ea0113b79728f387795c0e09a51743c0dee8
Berkan YILMAZ <55620672+berkanyilmz@users.noreply.github.com>
2022-07-25 16:22:58 +0300

    Add files via upload
    An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists).
    Given an array arr[] of size N, Return the index of any one of its peak elements.

---
+++ PeakElement/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    static int peakElement(int[] arr, int n)
+    {
+        if (n == 1) {
+            return 0;
+        }
+        if (arr[0] >= arr[1]) {
+            return 0;
+        }
+        if (arr[n - 1] >= arr[n - 2]) {
+            return n - 1;
+        }
+        for (int i = 1; i < n-1; i++) {
+            if (arr[i-1] < arr[i] && arr[i] > arr[i+1]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10, 20, 15, 2, 23, 90, 67};
+        System.out.println(peakElement(arr, arr.length));
+    }
+}
